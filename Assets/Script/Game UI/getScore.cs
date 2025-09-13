63ee565a77396c397311a925524e05c4df7335c3
LEE_SANG_JUN <sahwang04@gmail.com>
2025-09-13 23:14:56 +0900

    애니메이션 효과 적용

--- Assets/Script/Game UI/getScore.cs
+++ Assets/Script/Game UI/getScore.cs
@@ -1,7 +1,5 @@
-using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class getScore : MonoBehaviour
 {
@@ -11,39 +9,64 @@ public class getScore : MonoBehaviour
 
     public float moveSpeed = 5f;
     public float stopYPos = 0f;
+    public float destroyTime = 1f;
+
+    private float animationTimer;
+    private bool isAnimating = false; // 애니메이션이 진행 중인지 확인하는 플래그
+
 
     void Start()
     {
-        getscore.text = "";
-        startPos = getscore.gameObject.transform.position;
+        getscore.gameObject.SetActive(false);
+        startPos = getscore.gameObject.transform.localPosition;
         player = FindFirstObjectByType<Player_Move>();
     }
 
-    void Update()
-    {
-
-    }
-
-    void FixedUpdate()
+    void LateUpdate()
     {
         transform.position = player.transform.position;
-        
+        if (isAnimating)
+        {
+            getscore.gameObject.transform.localPosition += Vector3.up * moveSpeed * Time.deltaTime; ;
+            animationTimer += Time.deltaTime;
+
+            Color newColor = getscore.color;
+            newColor.a = 1 - (animationTimer / destroyTime);//숫자가 희미해짐
+            getscore.color = newColor;
+
+            // 애니메이션이 끝나면 리셋합니다.
+            if (animationTimer >= destroyTime)
+            {
+                ResetAnimation();
+            }
+        }
     }
 
     public void lookscore(int sc)
     {
-        getscore.text = $"{sc}";   
+        ResetAnimation();
+
+        // 텍스트를 활성화하고 점수를 설정합니다.
+        if (sc > 0)
+        {
+            getscore.text = $"+{sc}";
+        }
+        else
+        {
+            getscore.text = $"{sc}";
+        }
+        
+        getscore.gameObject.SetActive(true);
+        isAnimating = true;
     }
 
-    // void moveGet()
-    // {
-    //     if (getfruit)
-    //     {
-    //         getscore.gameObject.transform.position = new Vector2(0, Time.deltaTime * moveSpeed);
-    //     }
-    //     else
-    //     {
-
-    //     }
-    // }
+    private void ResetAnimation()
+    {
+        isAnimating = false;
+        getscore.gameObject.SetActive(false);
+        getscore.gameObject.transform.localPosition = startPos;
+        // 텍스트 색상을 다시 불투명하게 만듭니다.
+        getscore.color = new Color(getscore.color.r, getscore.color.g, getscore.color.b, 1);
+        animationTimer = 0f;
+    }
 }
--- Assets/Script/Score System/Score.cs
+++ Assets/Script/Score System/Score.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 
 public class Score : MonoBehaviour
 {
+    private getScore getScore;
+
+    void Start()
+    {
+        // getScore = FindFirstObjectByType<getScore>();
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // getScore.mPostext = true;
         Debug.Log(collision.gameObject.name);
         GameManager.Instance.CheckFruitz(collision.gameObject.name);
         if (collision.gameObject.CompareTag("Fruit"))
