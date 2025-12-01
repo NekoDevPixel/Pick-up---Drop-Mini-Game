6f16ad11d8bde3e505d830316d59d25810a24fa2
LEE_SANG_JUN <sahwang04@gmail.com>
2025-12-01 23:06:59 +0900

    주석 제거

--- Assets/Script/MultiPlayer/PhotonServer.cs
+++ Assets/Script/MultiPlayer/PhotonServer.cs
@@ -19,7 +19,7 @@ public class PhotonServer : MonoBehaviourPunCallbacks
 
     public void btn()
     {
-         // 1. 게임 시작 시 자동으로 포튼 서버 접속
+        
         matchUI.Name[0].text = "서버 접속 중...";
         PhotonNetwork.ConnectUsingSettings();
     }
@@ -33,6 +33,7 @@ public class PhotonServer : MonoBehaviourPunCallbacks
         matchUI.Name[1].text = "";
 
         currentPlayerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        matchUI.Name[3].text = "";
         // matchUI.Name[0].text = $"현재 인원: {currentPlayerCount} / 2";
         // if(currentPlayerCount == 0)
         // {
@@ -42,22 +43,20 @@ public class PhotonServer : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
-        // 2. 서버 접속 완료되면 바로 방 찾기 시도
         matchUI.Name[0].text = "빈 방 찾는 중...";
         PhotonNetwork.JoinRandomRoom();
     }
 
-    // 3. 빈 방 찾기 실패 (방이 없거나 꽉 참) -> 내가 방을 새로 만듦
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         matchUI.Name[0].text = "빈 방이 없음. 새로운 방 생성...";
-        // 중요: 최대 인원을 2명으로 제한
+
         RoomOptions roomOptions = new RoomOptions { MaxPlayers = 2 };
         PhotonNetwork.CreateRoom(null, roomOptions);
         UpdatePlayerListUI();
     }
 
-    // 4. 방 입장 성공 (방장이든 참가자든 이 함수가 호출됨)
+
     public override void OnJoinedRoom()
     {
         currentPlayerCount = PhotonNetwork.CurrentRoom.PlayerCount;
@@ -67,24 +66,24 @@ public class PhotonServer : MonoBehaviourPunCallbacks
         UpdatePlayerListUI();
         if (currentPlayerCount == 2)
         {
-            // 내가 들어갔는데 이미 2명이 되었다면 바로 게임 시작
+
             StartGame();
         }
         else
         {
-            // 아직 나 혼자라면 대기
+
             matchUI.Name[0].text = "상대방 기다리는 중...";
         }
     }
 
-    // 5. 다른 플레이어가 방에 들어왔을 때 (내가 방장이고 기다리던 상황)
+
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         matchUI.Name[0].text = "상대방 입장!";
         matchUI.Name[3].text += $"{newPlayer.NickName}님이 입장하셨습니다.\n";
         UpdatePlayerCountUI();
         UpdatePlayerListUI();
-        // 방에 2명이 찼는지 확인
+
         if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
         {
             // StartGame();
@@ -94,7 +93,7 @@ public class PhotonServer : MonoBehaviourPunCallbacks
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         matchUI.Name[3].text += $"{otherPlayer.NickName}님이 퇴장하셨습니다.\n";
-        matchUI.Name[2].text = "상대방 없음";
+        matchUI.Name[2].text = "상대방 없음"; 
         UpdatePlayerCountUI();
     
     }
